bb76e3562802769e9361bc093199ce0630a8b0dd
Potashin Andrei <a.potashin@pnlfin.tech>
2024-12-10 17:36:32 +0200

    popular categories

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
-import { Hero } from "@/components/home";
 import Image from "next/image";
+import { Hero, PopularCategories } from "@/components/home";
 
 export default function Home() {
   return (
     <div className="">
       <main className="">
         <Hero />
+        <PopularCategories />
       </main>
     </div>
   );
---
+++ src/components/home/PopularCategories.tsx
@@ -0,0 +1,14 @@
+export const PopularCategories = () => {
+  return (
+    <section className="popular-categories bg-gray-100 py-16">
+      <div className="container mx-auto px-4 text-center">
+        <h2 className="text-3xl md:text-4xl font-bold mb-8">
+          Популярные категории
+        </h2>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+          Здесь позже будут карточки категорий
+        </div>
+      </div>
+    </section>
+  );
+};
