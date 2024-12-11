f684f18c85e37348a731c10aa6ca43756459d25d
Potashin Andrei <a.potashin@pnlfin.tech>
2024-12-11 19:34:42 +0200

    New arrivals component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 "use server";
 
 import { Hero, PopularCategories, SalesDiscounts } from "@/components/home";
+import NewArrivals from "@/components/home/NewArrivals";
 
 export default async function Home() {
   return (
@@ -9,6 +10,7 @@ export default async function Home() {
         <Hero />
         <PopularCategories />
         <SalesDiscounts />
+        <NewArrivals />
       </main>
     </div>
   );
--- src/components/home/index.ts
+++ src/components/home/index.ts
@@ -1,3 +1,4 @@
 export * from "./Hero";
 export * from "./PopularCategories";
 export * from "./SalesDiscounts";
+export * from "./NewArrivals";
